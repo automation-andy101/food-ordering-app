9c2d915ecbf92f97398772dc516c44dcfb433d40
automation-andy101 <automation.andy101@gmail.com>
2025-07-15 21:46:06 +0100

    Adding custom input and button components

--- app/(auth)/sign-in.tsx
+++ app/(auth)/sign-in.tsx
@@ -1,12 +1,40 @@
-import { router } from "expo-router"
-import React from 'react'
-import { Button, Text, View } from 'react-native'
+import CustomButton from '@/components/CustomButton';
+import CustomInput from '@/components/CustomInput';
+import { Link } from 'expo-router';
+import React from 'react';
+import { Text, View } from 'react-native';
 
 const SignIn = () => {
   return (
-    <View>
-      <Text>SignIn</Text>
-      <Button title="Sign In" onPress={() => router.push("/sign-up")} />
+    <View className="gap-10 bg-white rounded-lg p-5 mt-5">
+      <CustomInput 
+        placeholder='Enter your email'
+        value={''}
+        onChangeText={() => {}}
+        label='Email'
+        keyboardType='email-address'
+      />
+
+      <CustomInput 
+        placeholder='Enter your password'
+        value={''}
+        onChangeText={() => {}}
+        label='Password'
+        secureTextEntry={true}
+      />
+
+      <CustomButton 
+        title='Sign In'
+      />
+
+      <View>
+        <Text className='base-regular text-gray-100'>
+          Don't have an account?
+        </Text>
+        <Link href="/sign-up" className='base-bold text-primary'>
+          Sign Up
+        </Link>
+      </View>
     </View>
   )
 }
---
+++ lib/appwrite.ts
@@ -0,0 +1,7 @@
+export const appwriteConfig = {
+    endpoint: process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT,
+    platform: 'com.dd.foodorderingapp',
+    projectid: process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID,
+    databaseId: '68767752002117e4058a',
+    userCollectionId: '68767819003ded9bbbfc'
+}
